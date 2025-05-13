ea10b01306e0116ad73f3bda15610740a1a2fcde
SnU <82738567+SingularUnconventional@users.noreply.github.com>
2025-05-13 14:27:56 +0000

    2단계 완료

---
+++ test.py
@@ -0,0 +1,149 @@
+from dataclasses 	import dataclass
+
+import numpy as np
+
+WORLD_WIDTH_SCALE = 50
+WORLD_HIGHT_SCALE = 50
+
+PLAYERS_SIZE		= 50
+PLAYER_START_HP		= 100
+PLAYER_START_ENERGY	= 100
+PLAYER_START_SPEED	= 1
+PLAYER_BMR_ENERGY	= 3
+
+FOODS_SIZE			= 30
+FOOD_START_ENERGY	= 30
+
+SUBSTANCE_A_ENERGY	= 1
+
+
+
+class Vector2:...
+@dataclass
+class Vector2:
+	x : int
+	y : int
+
+	def __add__(self, vector : Vector2) -> Vector2:
+		return Vector2(self.x + vector.x, self.y + vector.y)
+
+	
+class World:
+	def __init__(self):
+		self.players: list[Player] 	= []
+		self.foods 	: list[_Food] 	= []
+
+		for _ in range(PLAYERS_SIZE):
+			player = Player(Vector2(
+							np.random.uniform(WORLD_WIDTH_SCALE), 
+							np.random.uniform(WORLD_HIGHT_SCALE)
+							))
+			
+			self.players.append(player)
+		
+		for _ in range(FOODS_SIZE):
+			food = _Food()
+			food.regenerate()
+
+			self.foods.append(food)
+			
+	def Trun(self):
+		for player in self.players:
+			player.move()
+
+		self.players = [player for player in self.players if player.alive]
+
+	def __str__(self):
+		grid = [["⬛"
+				for _ in range(WORLD_WIDTH_SCALE)]
+		  		for _ in range(WORLD_HIGHT_SCALE)]
+		
+		for player in self.players:
+			yPos = int(player.position.y % WORLD_WIDTH_SCALE)
+			xPos = int(player.position.x % WORLD_HIGHT_SCALE)
+			
+			grid[yPos][xPos] = "🟦🟪⬜🟨🟧🟥"[int((5*player.energy)/(player.energy+1000))]
+
+		for food in self.foods:
+			yPos = int(food.position.y)
+			xPos = int(food.position.x)
+			
+			grid[yPos][xPos] = "🟩"
+
+		return "⬜️"*WORLD_WIDTH_SCALE+"\n"+"\n".join(["".join(x_line) for x_line in grid])
+
+
+class Player:
+	def __init__(self, position : Vector2):
+		self.position:Vector2	= position
+		self.hp		: int 		= PLAYER_START_HP
+		self.energy	: int 		= PLAYER_START_ENERGY
+		self.alive 	: bool		= True
+	
+	def move(self):
+		closestFood = world.foods[
+		find_closest_point_arg(
+			points=np.array([[food.position.x, food.position.y] for food in world.foods]),
+			target=np.array([self.position.x, self.position.y])
+		)]
+
+		foodLocalPosX = closestFood.position.x - self.position.x
+		foodLocalPosY = closestFood.position.y - self.position.y
+
+		Scale = PLAYER_START_SPEED / (np.abs(foodLocalPosX) + np.abs(foodLocalPosY))
+
+		self.position += Vector2(foodLocalPosX*Scale, foodLocalPosY*Scale)
+		self.eat()
+
+		if True: self.energy += SUBSTANCE_A_ENERGY
+		self.energy -= PLAYER_BMR_ENERGY
+		if self.energy < 0:
+			self.alive = False
+
+
+	def eat(self):
+		foodLength, foodNum = find_closest_point(
+			points=np.array([[food.position.x, food.position.y] for food in world.foods]),
+			target=np.array([self.position.x, self.position.y])
+		)
+
+		food = world.foods[foodNum]
+
+		if foodLength < 1:
+			self.energy += food.energy
+			food.regenerate()
+
+	def dead(self):
+		print('dead')
+		del self
+
+
+#임시
+class _Food:
+	def __init__(self):
+		self.position	: Vector2
+		self.energy		: int = FOOD_START_ENERGY
+
+	def regenerate(self):
+		self.position = Vector2(
+						np.random.uniform(WORLD_WIDTH_SCALE), 
+					   	np.random.uniform(WORLD_HIGHT_SCALE))
+		
+def find_closest_point_arg(points: np.array, target: np.array) -> int:
+	dists = np.sum((points - target)**2, axis=1)
+	return np.argmin(dists)
+
+def find_closest_point(points: list[Vector2], target: Vector2):
+	dists = np.sum((points - target)**2, axis=1)
+	return np.min(dists), np.argmin(dists)
+
+def Loop():
+	world.Trun()
+
+	input(world)
+
+if __name__ == "__main__":
+	np.random.seed(394900038)
+	world = World()
+	while True:
+		Loop()
